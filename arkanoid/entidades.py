3cc561da10ce9435c533b8fa88588aeb07fa665a
Seven-z01 <Seven-z01@outlook.com>
2021-09-03 21:57:36 +0200

    Creación de Bola, Tiles, colisiones y UI

--- arkanoid/entidades.py
+++ arkanoid/entidades.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from pygame.sprite import Sprite
+from . import ANCHO, ALTO
 
 
 class Raqueta(Sprite):
@@ -10,8 +11,33 @@ class Raqueta(Sprite):
         self.rect = self.image.get_rect(**kwargs)
 
     def update(self):
-        if pg.key.get_pressed()[pg.K_LEFT]:
-            self.rect.x -= 5
+        if pg.key.get_pressed()[pg.K_LEFT] and not self.rect.x <= 0:
+            self.rect.x -= 7
 
-        if pg.key.get_pressed()[pg.K_RIGHT]:
-            self.rect.x += 5
+        if pg.key.get_pressed()[pg.K_RIGHT] and not self.rect.x >= ANCHO - self.rect.w:
+            self.rect.x += 7
+
+
+class Bola(Sprite):
+    disfraces = "ball1.png"
+
+    def __init__(self, **kwargs):
+        self.image = pg.image.load(f"resources/images/{self.disfraces}")
+        self.rect = self.image.get_rect(**kwargs)
+        self.dx = 7
+        self.dy = 7
+
+    def update(self):
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+        if self.rect.x + self.rect.w > ANCHO or self.rect.x < 0:
+            self.dx *= -1
+        if self.rect.y < 0:
+            self.dy *= -1
+
+class Tiles(Sprite):
+    disfraces = "redTile.png"
+
+    def __init__(self, **kwargs):
+        self.image = pg.image.load(f"resources/images/{self.disfraces}")
+        self.rect = self.image.get_rect(**kwargs)
